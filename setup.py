246ade92c9f8bc7db7c175afee124439b9872cc2
Rollo Konig-Brock <rollokb@gmail.com>
2015-05-22 20:07:33 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 
 setup(
+    packages=['jsonapi_serializers'],
     name='jsonapi_serializers',
-    version='0.0.0',
+    version='0.0.1',
     description='A serializer package for django rest framework that assists in the creation of json apis which conform standards like http://jsonapi.org/',
     author='Rollo Konig-Brock',
     author_email='rollokb@gmail.com',
     url='https://github.com/rollokb/jsonapi-serializers',
     keywords=['django', 'restframework', 'api', 'jsonapi'],
-    zip_safe=False
+    
 )
